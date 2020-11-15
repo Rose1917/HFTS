5c84be265fe431ebfae54a3a5f128a4e57ba9fd1
xiaoxin-15 <renyanjie01@gmail.com>
2020-11-15 11:00:39 +0800

    add the util file

---
+++ include/util.h
@@ -0,0 +1,7 @@
+#ifndef UTIL_H
+#define UTIL_H
+#define ARRAY_SIZE 30
+char* double2c(double);
+char* float2c(float);
+char* int2c(int);
+#endif
---
+++ util.cpp
@@ -0,0 +1,16 @@
+#include "include/common.h"
+char* double2c(double d){
+    char* temp_str=(char*)malloc(ARRAY_SIZE);
+	sprintf(temp_str,"%.4lf",d);
+    return temp_str;
+}
+char* int2c(int i){
+    char* temp_str=(char*)malloc(ARRAY_SIZE);
+	sprintf(temp_str,"%d",i);
+    return temp_str;
+}
+char* float2c(float f){
+    char* temp_str=(char*)malloc(ARRAY_SIZE);
+	sprintf(temp_str,"%.4f",f);
+    return temp_str;
+}
